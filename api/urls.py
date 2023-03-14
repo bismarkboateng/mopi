cb7d807a1d57e125ad815820243f3a4ba39b6582
bismarkboateng <bismarkb609@gmail.com>
2023-03-14 22:47:45 +0000

    added endpoint to get all movies

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path("", views.allEndpoints, name="index")
+    path("", views.allEndpoints, name="index"), 
+    path("all/", views.mopi_all, name="all movies")
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,16 @@
 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.request import Request
+from .models import Movie
+from .serializers import MovieSerializer
+from rest_framework import status
 
 # Create your views here.
+
+def get_objects():
+    return Movie.objects.all()
+
 
 @api_view(['GET'])
 def allEndpoints(request):
@@ -16,3 +24,11 @@ def allEndpoints(request):
     return Response(Endpoints)
 
 
+
+
+@api_view(['GET'])
+def mopi_all(request:Request):
+    movies = get_objects()
+    serializer = MovieSerializer(movies, many=True)
+    
+    return Response(serializer.data, status=status.HTTP_200_OK)
